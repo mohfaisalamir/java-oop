b5304ab4afdeedc74dab3af25d46a580675d2da6
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-02-29 13:07:59 +0700

    OOP stands for Object-Oriented Programming

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        for (int i = 0; i < 30; i++) {
+            System.out.println("Diancok " +Math.random()*100);
+        }
+    }
+}
