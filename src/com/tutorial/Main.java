8d1d31df4a72851b1592e4fd25f2004c08c42494
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-02-29 15:08:02 +0700

    pengenalan class dan object

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,11 +1,40 @@
 package com.tutorial;
+class Mahasiswa { // membuat kelas
+    String nama;
+    String jurusan;
+    String NIM;
+    double IPK;
+    Integer angkatan;
 
+}
 public class Main {
     public static void main(String[] args) {
+        // instasnsiasi  / membuat objek
 
-        System.out.println("Hello world!");
-        for (int i = 0; i < 30; i++) {
-            System.out.println("Diancok " +Math.random()*100);
-        }
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.nama      = "Imron Sofyan";
+        mahasiswa.NIM       = "3714100036";
+        mahasiswa.jurusan   = "Teknik Sipil";
+        mahasiswa.IPK       = 2.99;
+        mahasiswa.angkatan  = 2011;
+
+        System.out.println(mahasiswa.nama);
+        System.out.println(mahasiswa.NIM);
+        System.out.println(mahasiswa.jurusan);
+        System.out.println(mahasiswa.angkatan);
+        System.out.println(mahasiswa.IPK);
+
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        mahasiswa1.nama      = "Abdul Manab";
+        mahasiswa1.NIM       = "3714100034";
+        mahasiswa1.jurusan   = "Teknik Sipil";
+        mahasiswa1.IPK       = 3.67;
+        mahasiswa1.angkatan  = 2009;
+
+        System.out.println(mahasiswa1.nama);
+        System.out.println(mahasiswa1.NIM);
+        System.out.println(mahasiswa1.jurusan);
+        System.out.println(mahasiswa1.angkatan);
+        System.out.println(mahasiswa1.IPK);
     }
 }
