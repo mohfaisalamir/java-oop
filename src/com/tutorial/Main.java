91dd82c29eebc6e4adeacd5c46b9fccc0853223a
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-02-29 15:13:39 +0700

    Menulis catatan pembelajaran

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -3,6 +3,11 @@ package com.tutorial;
 public class Main {
     public static void main(String[] args) {
 
+        // ini adalah catatan pemeblajaran saya
+        // dengan mentor bapak faqihza Kelas terbuka,
+        // untuk catatan selanjutnya anda bisa cek di branch atau repositori yang lainnya
+        // untuk kodi di dalam folder ini terdapat beberapa branc didalamnya..
+
         System.out.println("Hello world!");
         for (int i = 0; i < 30; i++) {
             System.out.println("Diancok " +Math.random()*100);
