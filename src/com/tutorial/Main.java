af915dc2701ce7e0af7076173b7d1a14ba9d0ba7
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-02 23:59:53 +0700

    05 belajar OOP Method

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,48 +1,79 @@
-package com.tutorial;
+package  com.tutorial;
 
-//class tanpa constructor / class polos
-class Polos {
-    String dataString;
-    Integer dataInteger;
-}
 class Mahasiswa{
-    String nama;
+    // Member (Atribut)
+    String nama  ="tambun";
     String NIM;
     String jurusan;
-    Mahasiswa(String inputNAma, String inputNIM, String inputJurusan){
-        //adalah fungsi Baawaan(sebenrnya), yang dipanggil pertamakali ketika Objek Dibuat
-        nama=inputNAma;
-        NIM=inputNIM;
-        jurusan=inputJurusan;
-        System.out.println("Ini adalah Condtructor");
+    // Constructor (method yang ada/devault yang terpanggil sesaat object dibuat) method yang pertama trpanggil saat objek dijalankan
+    Mahasiswa(String nama, String NIM, String jurusan){
+        this.nama=nama;
+        this.NIM=NIM;
+        this.jurusan=jurusan;
+       // Main.test();
+    }
+    //METHOD TANPA RETURN DAN TANPA PARAMETER
+    void show(){
+        // method no-static, karna dia terikat class,
+        // jika method anda pakai static didalam kelas  maka error,
+        // static digunakan untuk membuat method inde[penden (tak terikat kelas, seperti PSVM)
+        System.out.println("Nama    : "+this.nama);
+        System.out.println("NIM     : "+this.NIM);
+    }
+    //METHOD TANPA RETURN DAN DENGAN PARAMETER
+    void setNama(String nama){ // ini nanti kaitannya dengan setter
+        this.nama=nama;
+    }
+    //METHOD DENGAN RETURN TAPI TANPA PARAMETER
+    String getNama(){ // ini nanti kaitannya dengan getter
+        return this.nama;
+    }
+    //METHOD DENGAN RETURN DAN DENGAN PARAMETER
+    String sayHello(String message){
+        return message +" Karo "+this.nama+ " iki lho, puancen muatane diancok, urip kok ra mbejaji blas!!! asu asu";
     }
 
     @Override
     public String toString() {
-        return "Mahasiswa{\n" +
+        return "Mahasiswa{" +
                 "nama='" + nama + '\'' +
-                ", \nNIM='" + NIM + '\'' +
-                ", \njurusan='" + jurusan + '\'' +
+                ", NIM='" + NIM + '\'' +
+                ", jurusan='" + jurusan + '\'' +
                 '}';
     }
 }
-
+class Dosen{
+    String name;
+    Dosen(String name){
+        this.name=name;
+    }
+}
 public class Main {
-        public static void main(String[] args) throws Exception {
-//            Polos objectPolos = new Polos();
-//        objectPolos.dataString="diancok";
-//        objectPolos.dataInteger=20;
-//
-//            Polos polos = new Polos();
-//        polos.dataString="PKI Jancok";
-//        polos.dataInteger=30;
-//            System.out.println(polos.dataString);
-//            System.out.println(polos.dataInteger);
-//            System.out.println(objectPolos.dataString);
-//            System.out.println(objectPolos.dataInteger);
-            Mahasiswa mahasiswa = new Mahasiswa("Gatel","900011","Kimia");
-            System.out.println(mahasiswa.toString());
-            }
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa("amir","371900023","biologi");
+        // nyoba method getName
+        System.out.println("get name : "+mahasiswa.getNama());
 
-        }
 
+        //System.out.println("ini milik main :"+mahasiswa.nama);// mahasiswa.nama ini setara dengan this.nama
+        //memanggil method tanpa return dan tanpa parameter
+        mahasiswa.show();
+        //memanggil method tanpa return dan tanpa parameter
+        mahasiswa.setNama("Usman");
+        mahasiswa.show();
+        //memanggil method dnegan return dan tanpa parameter
+        System.out.println("get name : "+mahasiswa.getNama());
+        //memanggil method dnegan return dan dengan parameter
+        System.out.println(mahasiswa.sayHello("Jalal"));
+
+        // coba manggil dosen dengan menguji keberadaan fungsiny (YANG sebenarnya gak ada sih)
+        Dosen dosen = new Dosen("Alex");
+        //dosen.syHello(); jika seperti ini maka akan error, karena sayHello bukan si fungsi si Dosen
+
+
+        System.out.println("\n"+mahasiswa.toString());
+    }
+    static void test(){
+        System.out.println("Jiancok asuuu! ");
+    }
+}
