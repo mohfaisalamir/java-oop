21a591ef3ef06ed39a68c8ca8191ce6fa0cb29a2
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-07 14:31:40 +0700

    Nambah catatan

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         // ini adalah catatan pemeblajaran saya
-        // dengan mentor bapak faqihza Kelas terbuka,
-        // untuk catatan selanjutnya anda bisa cek di branch atau repositori yang lainnya
-        // untuk kodi di dalam folder ini terdapat beberapa branc didalamnya..
+        // dengan mentor bapak faqihza, di channel youtube Kelas terbuka,
+        // untuk catatan selanjutnya anda bisa cek di branch  atau repositori saya yang lainnya
+        // untuk koding di dalam folder ini terdapat beberapa branc didalamnya..
 
         System.out.println("Hello world!");
         for (int i = 0; i < 30; i++) {
-            System.out.println("Diancok " +Math.random()*100);
+            System.out.println("Dianzoo " +Math.random()*100);
         }
     }
 }
