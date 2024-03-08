a1b797c253138fad2d93f976c2b535e26d75504f
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-08 14:24:30 +0700

    09-public-and-private-keywords

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,56 +1,60 @@
-package com.tutorial;
+package  com.tutorial;
+class Player{
+    private String nama;   // (tanpa access modifier (Privat, oblick or other)) ini default,
+    // artinya dia bisa dibaca dab bisa ditulis kapanpun.
+    public int umur; // public, bisa dibaca dan ditulis di luar kelas
+    private int health; // private, hanya bisa diakses di dalam kelas saja ,
+    // jika dipakasa maka gagal compiling alias error
 
-class Book{
-    String title;
-    String author;
-    Book(String judul, String penulis){
-        title = judul;
-        author= penulis;
+
+    public Player(String nama, int umur, int health) {
+        this.nama = nama;
+        this.umur = umur;
+        this.health = health;
     }
     void display(){
-        System.out.println("\nJudul Buku  : "+this.title);
-        System.out.println("Penulis     : "+this.author);
-    }
-}
+        // apapun yang diprivate seperti fungsi setHealth(), maka hanya bisa diakases didalam kelas seperti berikut
+        setHealth();
+        System.out.println("\nNama      : "+this.nama);
+        System.out.println("Umur        : "+this.umur);
+        System.out.println("Kesehatan   : "+this.health); //karena private
+        // maka dia hanya bisa diakases di sini (didalam kelas Player) ,
 
+    }
+    public void getName(String nama){
+        this.nama = nama;
+    }
+    // sekarang kita coba mem-privat-kan method (jadi ya hanya bisa dipakai di dalam kelas)
+     private void setHealth(){
+        this.health += 200;
+    }
+
+}
 public class Main {
     public static void main(String[] args) {
-        Book buku = new Book("Das Kapital","Karl Marx");
-        buku.display();
-        // menampilkan address
-        String addressBook = Integer.toHexString(System.identityHashCode(buku));
-        System.out.println(addressBook + " ini adalah alamat memori dari : "+buku.title);
+        Player player = new Player("Marnox",34,22);
+        player.display();
 
-        // assigment object
-        Book buku1 = buku;
-        // kondisi seperti ini , object baru tidak menduplikat,
-        // melainkan mengalatkan memori pada alamat/referensi yang sama
-        buku1.display();
+        //DEFAULT
+//        System.out.println(player.nama); // membaca data
+//        player.nama = "Surtix"; // menulis data
+//        System.out.println(player.nama); // membaca data
+        // ini ketika player.name dipaksa, maka dia gagal compile, karena property "String name" di ubah menjadi prrivate
 
-        String addressBook1 = Integer.toHexString(System.identityHashCode(buku1));
-        System.out.println(addressBook1 + " ini adalah alamat memori dari : "+buku1.title);
+        //PUBLIC
+        System.out.println(player.umur); // membaca data
+        player.umur = 33; // menulis data
+        //player.health = 12; error==> tidak bisa di compile
+        System.out.println(player.umur); // membaca data
+        //System.out.println(player.health); error==> tidak bisa di compile
+        player.display(); // meskipun didalam display() terdapa atribut Private,
+        // namun sifatnya akan mengikuti fungsi yang membawanya
+        player.getName("Muldoko");// kendati, property di ganti Private, dia tetap bisa diakses (nanti kita belajar enkapsulasi)
+        player.display();
 
-        buku1.title = "Manifesto communist";
+        //player.setHealth();// gagal compile karena function saya ubah menjadi private, sehingga hanya bisa di pakai di dalam kelasnya saja..
 
-        System.out.println("\nsetelah perubahan maniferto communist");
-        // karena buku1 dan buku memiliki alamat yang sama, maka nilai nya akan mengikuti satu samalain
-        buku.display();
-        buku1.display();
 
-        fungsi(buku);
-        buku.display();
-        buku1.display();
-
-    }
-    static void fungsi(Book dataBuku){
-        // argument yang di lempar (pass by) di sini ini bukan objeck, melainkan reference
-        // bagaimanapun juga objek ini merujuk pada objek diatas yang mana akan menuju pada reference yang sama
-        // beda halnya dengan data primitif dia akan menduplikat, jika data objek dia akan merujuk ke alamat yang sama
-        // maka penting kita harus meng-encapsulation (membungkus dengan pengaman(pengaman yang kita buat sendiri sih sebenernya))..
-
-        String addressBUku = Integer.toHexString(System.identityHashCode(dataBuku));
-        System.out.println("\n"+addressBUku + " ini adalah alamat memori dari : "+dataBuku.title +" (DALAM FUNGSI)");
-        dataBuku.author= "tan malaka";
 
     }
 }
