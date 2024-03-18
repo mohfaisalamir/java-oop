df0986167c6b12719bbb81d69ab518769a0a5f61
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-18 13:33:44 +0700

    13-static-atau-class-attributes

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,178 +1,55 @@
-package com.tutorial;
-
-class Player{
+package  com.tutorial;
+class Display{
+    // variable atau atribut yang bersifat static maka nilainya akan di sharing ke semua object
+    // bahkan di class ==> ini bisa disebut jiga  class variable, variable yang diam dan nilainya terdapat pada semuanya
+    // jika satu atribut pada object atau class bahkan, jika di re-assign maka nilainya semua berubah.. sesuai dengan yang diubah
+    public static String type = "display";
     private String name;
-    private int baseHealth;
-    private int baseAttack;
-    private int incrementHealth;
-    private int incrementAttack;
-    private int level;
-    private int totalDamage;
-    private boolean isDie;
-
-    // ini adalah object
-    private Armor armor;
-    private Weapon weapon;
-    public String getName(){
-        return this.name;
+    Display(String name){
+        this.name = name ;
     }
-    public Player(String name){
-        this.name = name;
-        this.baseHealth = 100;
-        this.baseAttack = 210;
-        this.incrementHealth = 20;
-        this.incrementAttack = 20;
-        this.level = 1;
-        this.isDie = false;
-
+    void show(){
+        System.out.println("Nama : "+this.name);
     }
-
-
-    public  int getHealth(){
-        return this.maxHealth()- this.totalDamage;
-    }
-    public void attack(Player opponent){
-        // hitung damage
-        int damage = this.getAttackPower();
-        //print event
-        System.out.println("\n"+this.getName() + " attacking " + opponent.getName() + " with damage "+damage);
-        //this. (objek ini) sama dengan opponent, yaitu sama sama merujuk pada kelas yang sama "Player"
-        // ruwet, berhubung kelas hanya satu, maka masing2 player bisa mengklaim dan mengubah atribut sesuai kehendak object asing masing
-        // ynag idealnya harus memeiliki kelas masing2
-        // sekarang player (this) attacking player lainnya (opponent)
-
-        //tiap satuserangnan maka player nambah satu level
-        this.levelUp();
-
-        opponent.defence(damage);
-
-    }
-    public void defence (int damage){
-        //receive damage
-        int defencePower = this.armor.getDefencePower();
-        int deltaDamage;
-
-        System.out.println(this.getName() + " defence power "+defencePower);
-
-        if(damage > defencePower){
-            deltaDamage = damage - defencePower;
-        }else {
-            deltaDamage = 0;
-        }
-        //this.totalDamage = this.totalDamage + deltaDamage;
-        //ditulis gini juga boleh
-        //ADDING total damage
-        this.totalDamage += deltaDamage;
-        System.out.println("damage earned is : "+deltaDamage+"\n");
-        // checking alive
-        if (this.getHealth() <= 0){
-            this.isDie = true;
-            this.totalDamage = this.maxHealth();
-        }
-        this.display();
-    }
-    private int getAttackPower(){
-        return this.baseAttack + this.level * this.incrementAttack + this.weapon.getAttack();
-    }
-    void display(){
-        System.out.println("\nPlayer\t\t\t: "+this.name);
-        System.out.println("Level\t\t\t: "+this.level);
-        System.out.println("Health\t\t\t: "+this.getHealth()+"/"+this.maxHealth());
-        System.out.println("Maximum Attack\t: "+this.getAttackPower());
-        System.out.println("Is Dead ?\t\t: "+this.isDie+"\n");
-        System.out.println("base attack, " +this.baseAttack);
-        System.out.println("increament attack, " +this.incrementAttack);
-        System.out.println("weapon attack, " +this.weapon.getAttack());
-    }
-    void setArmor(Armor armor){
-        this.armor = armor;
-    }
-    void setWeapon(Weapon weapon){
-        this.weapon=weapon;
-    }
-    int getAttack(){
-        return this.baseAttack;
-    }
-    public int maxHealth(){
-        return this.baseHealth +this.level * this.incrementHealth + this.armor.getAddHealth();
-    }
-    private void levelUp(){
-        this.level++;
+    void setType(String typeInput){
+        //type = typeInput; // alternatif 1 ,pemanggilan bisa pakai cara lain, jika ini kita gak tau type punya siapa, ini kebbetulan masih simpel jadi gak ribet
+        //this.type = typeInput; // alternatif 2 ,ini lebih jelas siapa pemiliknya, karena this ini merepesentasiakan object saat ini
+        Display.type = typeInput; // alternatif 3 , paling recomended klarena meminimalisir keambiguan, sehinga pakai Class bukan object sebagi media re-assign
     }
 }
-class Weapon{
-    private String name;
-    private int attack;
-    public Weapon(String name, int attack){
-        this.name = name;
-        this.attack = attack;
-    }
-    public int getAttack(){
-        return this.attack;
-    }
-}
-class Armor{
-    private String name;
-    private int strength;
-    private int health;
-    public Armor(String name, int strength, int health){
-        this.name = name;
-        this.strength = strength;
-        this.health=health;
-    }
-    public int getAddHealth(){
-        return this.health + this.strength * 10;
-    }
-    public  int  getDefencePower(){
-        return  this.strength*2;
-    }
-}
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        // player.baseHealth; karena di-private ini tak bisa di compile
-        Player player1=new Player("Minak Jinggo"); //PLAYER 1
-        Weapon weapon1 = new Weapon("Paser Wongko",18);
-        Armor armor1 = new Armor("Sapu Jagat",16,108);
-        player1.setArmor(armor1);
-        player1.setWeapon(weapon1);
+        System.out.println("test");
+
+        Display display = new Display("Aidit");
+        display.show();
+        Display display1= new Display("Muso");
+        display1.show();
+        Display display2= new Display("Muso");
+        Display display3= new Display("Muso");
+        Display display4= new Display("Muso");
+        Display display5= new Display("Muso");
 
 
-        //player.baseHealth = 0; tetep gak bisa diakses, harus dibuatkan fungsi yang public untuk mengaksesnya
-        Player player = new Player("Muldoko"); //PLAYER 2
-        Weapon weapon = new Weapon("tepel Manunggal", 21);
-        Armor armor = new Armor("rasuk Wojo",9,160);
-        player.setWeapon(weapon);
-        player.setArmor(armor);
+        display4.type = "Al Mujahid";// harus nya yang bernilai Al Mujahid cukup display4.type, tapi semua kena share karen bersifat static
+        // anda mau re-assign atribut satic bisa pakai object, namun alangkah baiknya jika pakai class, kenapa? , cari di google! wkwk
+        // berikut contohnya..
+        display2.type = "Muawiyah"; // jika di re-assign maka variable sebelumnya tampak tidak berarti (jadi abu2 seperti mati)
+        Display.type = "Al Anshor"; // maka outputnya Al Anshor
 
-        //tampilkan
-        System.out.println("\nKondisi sebelum perang");
-        player.display();
-        player1.display(); // sebelum level up
+        // jika di print maka hasilnya sama yaitu mengikuti reassign yang terakhir,
+        // dan bernilai sama karena static itu untuk men-share nilai ke semua object (salah satu metode management memory
+        // untuk keperluan tertentu)..
 
-        //PERANG
-        System.out.println("\nKondisi setelah perang");
-        player1.attack(player);
-        player.attack(player1);
-        player.attack(player1);
-        player.attack(player1);
-        player.attack(player1);
-        player.attack(player1);
-        player.display();// muldoko naik 6 level karenya nyerang 6 kali
-
-
-
-
-        //player1.attack(player);
-        // player.attack(player);
-
-        // coba lij=hat hasil perubahan
-
-        // PR : coba bikin saling serang, saat player (A) menyerang player(B) lain,
-        // maka A naik satu level
-
-        // evaluasi terakhir
-        // , karena this. dan opponet berada dalam satu kelas, maka masing2 object dapat mengubah masing2 atribut
-        // untuk keamanan ini tidak ideal , penuh kecacatan,
-        // nanti akan diperbaiki entah dengan membuat kelas baru sebagai Opponent opponent atau pakai cara yanng lainnya,..
+        // menampilkan static atau class variable
+        display1.setType("Muawiyah"); // aku coba re-assign lagi, semua output jadi "Muawiyah"
+        System.out.println("\n menampilkan static atau class variable");
+        System.out.println(display.type);// output "Al Mujahid"
+        System.out.println(display1.type);// output "Al Mujahid", jika pada variable pada umumnya (non-static) maka ini tetep bernilai "display"
+        System.out.println(display2.type);
+        System.out.println(display3.type);
+        System.out.println(display4.type);
+        System.out.println(display5.type);
+        System.out.println("dedengkot : "+Display.type);// ini babonnya, ini bentuk ideal untuuk misal re-assign
     }
 }
