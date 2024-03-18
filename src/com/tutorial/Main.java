117215f82a6fb7c27e3a87a3130e76b4c48ead4d
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-18 16:18:51 +0700

    14-static-atau-class-methods

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,55 +1,44 @@
-package  com.tutorial;
-class Display{
-    // variable atau atribut yang bersifat static maka nilainya akan di sharing ke semua object
-    // bahkan di class ==> ini bisa disebut jiga  class variable, variable yang diam dan nilainya terdapat pada semuanya
-    // jika satu atribut pada object atau class bahkan, jika di re-assign maka nilainya semua berubah.. sesuai dengan yang diubah
-    public static String type = "display";
+package com.tutorial;
+import  java.util.*;
+class Player{
+    private static int numberOfPlayer; // jumlah player, static juga bisa digabung dengan acces modifier misal "private"
+    private static ArrayList<String> listOfPlayer = new ArrayList<>();
     private String name;
-    Display(String name){
-        this.name = name ;
+    Player(String name){
+        this.name = name;
+        //numberOfPlayer++;
+        //this.numberOfPlayer++;//juga bisa
+        Player.numberOfPlayer++;// recomended
+        this.listOfPlayer.add(this.name);
+    }
+    // bikin method statis
+    static ArrayList<String> getName(){
+        return Player.listOfPlayer;
+    }
+    static void shownumberOfPlayer(){ // dan ini jadi getter
+        System.out.println("Number of Player = "+Player.numberOfPlayer);
+    }
+    void setName(String name){
+        this.name = name;
     }
     void show(){
-        System.out.println("Nama : "+this.name);
-    }
-    void setType(String typeInput){
-        //type = typeInput; // alternatif 1 ,pemanggilan bisa pakai cara lain, jika ini kita gak tau type punya siapa, ini kebbetulan masih simpel jadi gak ribet
-        //this.type = typeInput; // alternatif 2 ,ini lebih jelas siapa pemiliknya, karena this ini merepesentasiakan object saat ini
-        Display.type = typeInput; // alternatif 3 , paling recomended klarena meminimalisir keambiguan, sehinga pakai Class bukan object sebagi media re-assign
+        System.out.println("Name : "+this.name );
+        System.out.println("punya teman sebanyak "+Player.numberOfPlayer);
     }
 }
-public class Main{
+class Main{
     public static void main(String[] args) {
-        System.out.println("test");
+        Player player = new Player ("Umar");
+        Player player1 = new Player ("Usman");
+        Player player2 = new Player ("Ali");
+        Player player3 = new Player ("Abbas");
+        Player player4 = new Player ("Sufyan");
+        Player player5 = new Player ("Hindun");
+        // untuk mengambik nama nama tersebut ksn susah sehingga perlu dibuat ArrayList (materi kedepan seharusnya)
 
-        Display display = new Display("Aidit");
-        display.show();
-        Display display1= new Display("Muso");
-        display1.show();
-        Display display2= new Display("Muso");
-        Display display3= new Display("Muso");
-        Display display4= new Display("Muso");
-        Display display5= new Display("Muso");
+        player2.show();
+        Player.shownumberOfPlayer();
+        System.out.println(Player.getName());
 
-
-        display4.type = "Al Mujahid";// harus nya yang bernilai Al Mujahid cukup display4.type, tapi semua kena share karen bersifat static
-        // anda mau re-assign atribut satic bisa pakai object, namun alangkah baiknya jika pakai class, kenapa? , cari di google! wkwk
-        // berikut contohnya..
-        display2.type = "Muawiyah"; // jika di re-assign maka variable sebelumnya tampak tidak berarti (jadi abu2 seperti mati)
-        Display.type = "Al Anshor"; // maka outputnya Al Anshor
-
-        // jika di print maka hasilnya sama yaitu mengikuti reassign yang terakhir,
-        // dan bernilai sama karena static itu untuk men-share nilai ke semua object (salah satu metode management memory
-        // untuk keperluan tertentu)..
-
-        // menampilkan static atau class variable
-        display1.setType("Muawiyah"); // aku coba re-assign lagi, semua output jadi "Muawiyah"
-        System.out.println("\n menampilkan static atau class variable");
-        System.out.println(display.type);// output "Al Mujahid"
-        System.out.println(display1.type);// output "Al Mujahid", jika pada variable pada umumnya (non-static) maka ini tetep bernilai "display"
-        System.out.println(display2.type);
-        System.out.println(display3.type);
-        System.out.println(display4.type);
-        System.out.println(display5.type);
-        System.out.println("dedengkot : "+Display.type);// ini babonnya, ini bentuk ideal untuuk misal re-assign
     }
 }
