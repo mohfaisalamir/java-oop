03890f7fc43dfc19ed46ad5f3bf4f8d1c4a68779
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-19 12:40:10 +0700

    nambah info, manfaat public pada class

--- src/com/terminal/Console.java
+++ src/com/terminal/Console.java
@@ -3,6 +3,8 @@ package com.terminal;
 public class Console { // kenapa public kali ini di tampilkan? ya, kita akan lanjutkan di pembelajaran selanjutnya
     // public ini agar bisa di ekspor ke lain package, jika tidak di public maka tidak visible (gak terdetek)
     // maka jika di run fungsi ini pada file di package lain terjadi error, karena dianggap proram asing (tak dapat dicompile)..
+    // intinya, tambah "public" sebelum keywords class agar bisa di import
+
     public static void cetak(String mokak){
         System.out.println(mokak);
     }
@@ -14,5 +16,7 @@ public class Console { // kenapa public kali ini di tampilkan? ya, kita akan lan
         // ya mungkin karena ini kosakata message sudah sewajarnya jadi pesan,
         // sehingga tak perlu di peringatkan untuk usser, coba anda lihat di class Main, pasti hanya mokak dan pesen saja yang tampil,
         // tidak dengan message, (info ini gak penting, cuman buat wawsan saja)
+
+
     }
 }
