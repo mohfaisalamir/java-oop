7c621694ed434558a58d30e731f164697336f0cf
MOH_FAISAL_AMIR <mohfaisalamir94@gmail.com>
2024-03-23 13:50:46 +0700

    22-inheritance-practice-test

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,24 +1,7 @@
 package com.tutorial;
-
-public class Main {
+public  class Main {
     public static void main(String[] args) {
-        System.out.println("\nhaloo");
-        Hero hero = new Hero();
-        HeroStrength hero1 = new HeroStrength();
+        System.out.println("Taek");
 
-
-        // display
-        hero.display();
-        hero1.display();
-
-        // kita coba panggil dummy method di hero1 tapi pakai super. maka ouotputnya adalah method dummy hero
-        // dummy strengt
-        // mengandung function dummy method hero, karena memakai super.dummyMethod()
-
-        // outputnya adalah
-//        Dummy Method Hero
-//        Dummy Method Strength
-//        smahalnya dengan atribut, jika dummyMethod pada strength dhiapus, maka this.dummyMethod akan merujuk ke dummyMethod milik Hero (superClass)
-//        walaupun dia pakai this.dummyMethod bukan super.dummyMethod..
     }
 }
